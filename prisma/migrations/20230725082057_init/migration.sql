e453a30f5261eeb4cccbd0ea821c520f594c70f2
mrizki-22 <mrizkibgr22@gmail.com>
2023-07-25 15:21:46 +0700

    prisma postgresql

---
+++ prisma/migrations/20230725082057_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Anggota" (
+    "id" SERIAL NOT NULL,
+    "nama" TEXT NOT NULL,
+    "fakultas" TEXT NOT NULL,
+    "prodi" TEXT NOT NULL,
+    "no_telp" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "alamat" TEXT NOT NULL,
+    "angkatan" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "Anggota_pkey" PRIMARY KEY ("id")
+);
