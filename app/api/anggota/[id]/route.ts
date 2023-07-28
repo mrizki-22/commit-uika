e434fbd20c39e4331f6309ab0168887bffb06fe5
mrizki-22 <mrizkibgr22@gmail.com>
2023-07-28 13:58:17 +0700

    create update route

---
+++ app/api/anggota/[id]/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// get anggota by id
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const id = params.id;
+  try {
+    const idAnggota = parseInt(id);
+    const data = await prisma.anggota.findUnique({
+      where: {
+        id: idAnggota,
+      },
+    });
+    return NextResponse.json(data, { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(e, { status: 500 });
+  }
+}
+
+// update anggota by id
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+  const id = params.id;
+  const body = await request.json();
+  try {
+    const idAnggota = parseInt(id);
+    const data = await prisma.anggota.update({
+      where: {
+        id: idAnggota,
+      },
+      data: body,
+    });
+    return NextResponse.json("Success", { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(e, { status: 500 });
+  }
+}
