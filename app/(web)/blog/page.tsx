65591fd5a7ee1a67609824e6dc5eb7e50aa06cf2
mrizki-22 <mrizkibgr22@gmail.com>
2023-08-13 13:26:42 +0700

    create search input

--- app/(web)/blog/page.tsx
+++ app/(web)/blog/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Button } from "@/app/components/ui/button";
+import { Input } from "@/app/components/ui/input";
+import { BsSearch } from "react-icons/bs";
 
 function page() {
-  return <div>blog</div>;
+  return (
+    <div className="container ">
+      <div className="flex w-full mx-auto max-w-sm items-center space-x-2">
+        <Input type="email" placeholder="Cari Artikel" />
+        <Button type="submit" variant="outline">
+          <BsSearch className="inline-block w-4 h-4 stroke-current" />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default page;
